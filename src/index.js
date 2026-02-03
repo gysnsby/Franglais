9cfb3d932f8b0fa178b723e154da31844c50cb70
gysnsby <44318355+gysnsby@users.noreply.github.com>
2026-02-03 00:21:01 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App";
 import "./index.css";
+import App from "./App";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
-root.render(<App />);
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// Enable PWA / installability
+serviceWorkerRegistration.register();
