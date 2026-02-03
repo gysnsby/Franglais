11609bcbdff64b40bc8073a028973f37a2195349
gysnsby <44318355+gysnsby@users.noreply.github.com>
2026-02-03 00:12:05 +0000

    Create serviceWorkerRegistration.js

---
+++ src/serviceWorkerRegistration.js
@@ -0,0 +1,11 @@
+// Based on CRA default service worker registration
+
+export function register() {
+  if ("serviceWorker" in navigator) {
+    window.addEventListener("load", () => {
+      navigator.serviceWorker
+        .register("/service-worker.js")
+        .catch(() => {});
+    });
+  }
+}
